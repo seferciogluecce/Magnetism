e3131001c97ef61ecbd55348367b217e25332a0c
Ece Sefercioğlu <seferciogluecce@users.noreply.github.com>
2019-09-06 16:23:11 +0300

    Code comments and tidy up

--- Assets/GetMagnetized.cs
+++ Assets/GetMagnetized.cs
@@ -1,207 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-   public enum RotateMode
-    {
-        lerp,
-        slerp,
-        lerpUn,
-        slerpUn
+﻿using UnityEngine;
 
-
-    }
 public class GetMagnetized : MonoBehaviour
 {
-   public RotateMode rm = RotateMode.lerp; 
-    public string Tag = "Magnet";
-    public float speed = 5;
+    public string Tag = "Magnet"; //objects to be puuled or repelled
+    public float charge = 3; //power the object has
+    float absCharge; //magnitude of power
+    float ChargeSign; //sign of power
+    public float moveSpeed = 5; //speeds to rotate and move
     public float rotSpeed = 5;
-    public float charge = 3;
-    public float RotThreshold = 1; 
+    public float RotThreshold = 1; //Thresholds to stop moving
     public float DisThreshold = 1;
-    bool looking = false;
-     Rigidbody rg;
-    Collider target;
-    float Distance;
+    float Distance; //distance between the object and the target 
+    float OneOverDistanceSquare; //element used to calculate magnetic power
+    Collider target; //the source object that to be pulled or repelled
+    Vector3 TargetDirection;
     void OnDrawGizmos()
     {
-        //Vector3 forwardUp = other.transform.position - transform.position;
-        //Vector3 forward = transform.forward;
-        // Draws a blue line from this transform to the target
-        if (target) { 
-        Gizmos.color = Color.blue;
-            Gizmos.DrawLine(transform.position, target.transform.position);
-        Gizmos.color = Color.red;
-
-        Gizmos.DrawLine(transform.position, transform.position + transform.forward.normalized *2);
+        if (target) 
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, target.transform.position); //draws the path between the target and the current object
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward.normalized * absCharge);  //draws the forward vector of the object
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        rg = this.GetComponent<Rigidbody>();
-        GetComponent<BoxCollider>().size *= Mathf.Abs(charge);// new Vector3(  ); 
+        absCharge = Mathf.Abs(charge); //absolute value of charge
+        ChargeSign = Mathf.Sign(charge); //sign of charge too decide repel or attraction
+        GetComponent<BoxCollider>().size *= absCharge; //gravitational field is linear to charge value as the size of the collider that creates the field
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other) 
     {
-        
-    }
-
-
-
-    private void OnTriggerStay(Collider other)
-    {
-        if(other.gameObject.tag == Tag)
+        if (other.gameObject.tag == Tag)//when a target object is in collision with the charged object
         {
-            Debug.Log("Collided to " + Tag);
-
-            //   transform.Translate(other.gameObject.transform.position,);
-            // rg.AddForce((transform.position - other.transform.position ) * speed * Time.deltaTime *  1 / Vector3.Distance(transform.position, other.gameObject.transform.position) * charge);
-            //    transform.position = Vector3.Lerp(transform.position, other.gameObject.transform.position, speed * Time.deltaTime * 1 / Vector3.Distance(transform.position, other.gameObject.transform.position));
-            //   transform.position = Vector3.MoveTowards(transform.position, other.gameObject.transform.position, speed * Time.deltaTime * 1/Vector3.Distance(transform.position, other.gameObject.transform.position) );
-            // transform.LookAt(other.gameObject.transform );
-            //  Quaternion target = Quaternion.LookRotation(transform.position - other.transform.position);
-            //   Quaternion.Slerp(transform.rotation, other.gameObject.transform.rotation, rotSpeed * Time.deltaTime);
-            // Quaternion.Lerp(transform.rotation, target, rotSpeed * Time.deltaTime);
-
-            // Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lTargetDir), Time.time * rotSpeed);
-             Distance = Vector3.Distance(transform.position, other.gameObject.transform.position);
-            target = other;
-            RotMain(other); 
-            //Rotate(other); //inplace great enough
-            looking = true;
-            if (Distance > DisThreshold && Distance != 0 && looking)
+            Distance = Vector3.Distance(transform.position, other.gameObject.transform.position); //distance between objects
+            target = other; //target is used for moving and rotation methods as a global variable 
+            if (Distance != 0) //if the objects are not in the same spot
             {
-                //MoveTowards(other);//not rotation great enough
-                Move(other);
+                OneOverDistanceSquare = 1 / (Distance * Distance); //global variable to calculate attraction power
+                TargetDirection = Vector3.Scale((target.transform.position - transform.position), new Vector3(1, 0, 1)).normalized; //the direction when the current object looks at the target object
+                Rotate();
+                if (Distance > DisThreshold)
+                {
+                    Move();
+                }
             }
-
         }
     }
-    private void MoveTowards(Collider other)
+
+    private void Move() //object is moved by the charge value times target direction
     {
-
-        transform.Translate(Vector3.Scale(((other.transform.position - transform.position)), new Vector3(1, 0, 1)) * speed * (1 / (Distance * Distance)) * Mathf.Abs(charge) * Time.deltaTime);
-
-    }
-    private void Move(Collider other)
-    {
-
-        //   transform.Translate(  transform.forward * speed * (1 / (Distance * Distance)) * Mathf.Abs(charge) * Time.deltaTime  ); //will be rotated opposite side forward is in the opposite
-        transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.Scale((other.transform.position - transform.position), new Vector3(1, 0, 1)).normalized * charge, speed * (1 / (Distance * Distance)) * Time.deltaTime);
-
+        transform.position = Vector3.Lerp(transform.position, transform.position + TargetDirection * charge, moveSpeed * OneOverDistanceSquare * Time.deltaTime);
     }
 
-    void RotMain(Collider other)
+    void Rotate() //object is rotated around the y axis to look at or having the target in the back according to the object's charge sign
     {
-
-        Vector3 forward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1));
-
-        Vector3 forwardUp = Vector3.Scale(other.transform.position - transform.position, new Vector3(1, 0, 1));
-        Quaternion newRotation = Quaternion.FromToRotation(forward, forwardUp * Mathf.Sign(charge));
-        //Debug.Log(name + " " + newRotation.eulerAngles);
-        //Debug.Log(newRotation.eulerAngles.magnitude );
-
-
-        if (newRotation.eulerAngles.magnitude > RotThreshold && Distance != 0 && rm == RotateMode.lerp)
+        Vector3 forward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1)); //forward vector of current object
+        Vector3 forwardUp = TargetDirection; //target rotation 
+        Quaternion newRotation = Quaternion.FromToRotation(forward, forwardUp * ChargeSign); //rotation calculated from current forward direction to target object's direction
+        if (newRotation.eulerAngles.magnitude > RotThreshold) //If the needed rotation bigger than the threshold object rotates
         {
-
-            transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));// * charge);
-                                                                                                                                                                // transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));                                                                                                               // transform.Rotate(Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance))).eulerAngles);
-                                                                                                                                                                // transform.Rotate(newRotation.eulerAngles*( (newRotation.eulerAngles.magnitude>=180)? -1:1 ) * rotSpeed * Time.deltaTime * (1 / (Distance * Distance))); //sıkıntılı
-            looking = false;
-        } else if (newRotation.eulerAngles.magnitude > RotThreshold && Distance != 0 && rm == RotateMode.slerp)
-        {
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));// * charge);
-                                                                                                                                                                // transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));                                                                                                               // transform.Rotate(Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance))).eulerAngles);
-                                                                                                                                                                // transform.Rotate(newRotation.eulerAngles*( (newRotation.eulerAngles.magnitude>=180)? -1:1 ) * rotSpeed * Time.deltaTime * (1 / (Distance * Distance))); //sıkıntılı
-            looking = false;
+            transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * OneOverDistanceSquare * absCharge * Time.deltaTime); //rotation done from current to target rotation
         }
-        else if (newRotation.eulerAngles.magnitude > RotThreshold && Distance != 0 && rm == RotateMode.slerpUn)
-        {
-
-            transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));// * charge);
-                                                                                                                                                                // transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));                                                                                                               // transform.Rotate(Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance))).eulerAngles);
-                                                                                                                                                                // transform.Rotate(newRotation.eulerAngles*( (newRotation.eulerAngles.magnitude>=180)? -1:1 ) * rotSpeed * Time.deltaTime * (1 / (Distance * Distance))); //sıkıntılı
-            looking = false;
-        }
-        else if (newRotation.eulerAngles.magnitude > RotThreshold && Distance != 0 && rm == RotateMode.lerpUn)
-        {
-
-            transform.rotation = Quaternion.LerpUnclamped(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));// * charge);
-                                                                                                                                                                // transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));                                                                                                               // transform.Rotate(Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance))).eulerAngles);
-                                                                                                                                                                // transform.Rotate(newRotation.eulerAngles*( (newRotation.eulerAngles.magnitude>=180)? -1:1 ) * rotSpeed * Time.deltaTime * (1 / (Distance * Distance))); //sıkıntılı
-            looking = false;
-        }
-        else
-        {
-            Debug.Log("Now looking " + name);
-            looking = true;
-        }
-
     }
-
-
-
-
-    void Rotate2(Collider other)
-    {
-
-        //Vector3 forwardUp = Vector3.Scale(other.transform.position - transform.position, new Vector3(1, 0, 1));
-        //Vector3 forward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1));
-        //Quaternion newRotation = Quaternion.FromToRotation(forward, forwardUp * Mathf.Sign(charge));
-        //transform.rotation = Quaternion.LerpUnclamped(transform.rotation, other.transform.rotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));// * charge);
-        transform.LookAt(other.transform.position);
-
-
-
-        Vector3 forward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1));
-
-        Vector3 forwardUp = Vector3.Scale(other.transform.position - transform.position, new Vector3(1, 0, 1));
-        Quaternion newRotation = Quaternion.FromToRotation(forward, forwardUp * Mathf.Sign(charge));
-        //Debug.Log(name + " " + newRotation.eulerAngles);
-        //Debug.Log(newRotation.eulerAngles.magnitude );
-        if (newRotation.eulerAngles.magnitude > RotThreshold && Distance != 0)
-        {
-            transform.Rotate(newRotation.eulerAngles * rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));
-          //  transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));// * charge);
-                                                                                                                                                                // transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));                                                                                                               // transform.Rotate(Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance))).eulerAngles);
-                                                                                                                                                                // transform.Rotate(newRotation.eulerAngles*( (newRotation.eulerAngles.magnitude>=180)? -1:1 ) * rotSpeed * Time.deltaTime * (1 / (Distance * Distance))); //sıkıntılı
-            looking = false;
-        }
-        else
-        {
-            Debug.Log("Now looking " + name);
-            looking = true;
-        }
-
-
-    }
-    void Rotate(Collider other)
-    {
-            Vector3 forward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1));
-
-        Vector3 forwardUp = Vector3.Scale( other.transform.position - transform.position, new Vector3(1, 0, 1));
-        Quaternion newRotation = Quaternion.FromToRotation(forward, forwardUp * Mathf.Sign(charge));
-        //Debug.Log(name + " " + newRotation.eulerAngles);
-        //Debug.Log(newRotation.eulerAngles.magnitude );
-        if (newRotation.eulerAngles.magnitude > RotThreshold && Distance != 0)
-        {
-
-              transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));// * charge);
-           // transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime * (1 / (Distance * Distance)));                                                                                                               // transform.Rotate(Quaternion.Lerp(transform.rotation, transform.rotation * newRotation, rotSpeed * Time.deltaTime * (1 / (Distance * Distance))).eulerAngles);
-            // transform.Rotate(newRotation.eulerAngles*( (newRotation.eulerAngles.magnitude>=180)? -1:1 ) * rotSpeed * Time.deltaTime * (1 / (Distance * Distance))); //sıkıntılı
-            looking = false;
-        }
-        else
-        {
-            Debug.Log("Now looking " + name);
-            looking = true;
-        }
-        }
 }
-;////
---
+++ Assets/MoveObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public int Speed = 44;
+
+    void Update()
+    {
+        MoveWithArrows();
+    }
+
+    void Translate(float x, float y, float z) //point transformation
+    {
+        this.transform.position += new Vector3(x, y, z);
+    }
+
+    private void MoveWithArrows()//point transformation
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            Translate(0, 0, -Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            Translate(0, 0, Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            Translate(Speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            Translate(-Speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
